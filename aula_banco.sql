6ed22676e854ff99efe505aa4bbaf24ef6567fe9
nogutiyuka <112450056+nogutiyuka@users.noreply.github.com>
2022-09-09 00:02:08 -0300

    Add files via upload

--- aula_banco.sql
+++ aula_banco.sql
@@ -13,7 +13,7 @@ id INT NOT NULL AUTO_INCREMENT
 ,ativo CHAR(1) NOT NULL DEFAULT 'S'
 ,data_cadastro DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 ,CONSTRAINT pk_estado PRIMARY KEY (id)
-,CONSTRAINT coluna_ativo_deve_ser_S_ou_N CHECK (ativo IN ('S', 'N'))
+,CONSTRAINT estado_ativo_deve_ser_S_ou_N CHECK (ativo IN ('S', 'N'))
 );
 
 INSERT INTO estado (nome, sigla)
@@ -24,6 +24,24 @@ VALUES ('SÃO PAULO', 'SP');
 
 SELECT id, nome, sigla, ativo, data_cadastro FROM estado;
 
+DROP TABLE IF EXISTS cidade;
+
+CREATE TABLE cidade (
+id INT NOT NULL AUTO_INCREMENT
+,nome VARCHAR(200)  NOT NULL
+,ativo CHAR(1) NOT NULL DEFAULT 'S'
+,data_cadastro DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+,estado_id INT NOT NULL
+,CONSTRAINT pl_cidade PRIMARY KEY (id)
+,CONSTRAINT fk_cidade_estado FOREIGN KEY (estado_id) REFERENCES estado (id)
+,CONSTRAINT cidade_ativo_deve_ser_S_ou_N CHECK (ativo IN ('S', 'N'))
+,CONSTRAINT cidade_unica UNIQUE (nome, estado_id)
+);
+
+INSERT INTO cidade (nome, estado_id)
+VALUES ('CURITIBA', 1);
+
+SELECT * FROM cidade;
 
 
 
